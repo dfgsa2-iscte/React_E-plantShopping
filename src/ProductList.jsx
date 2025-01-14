c826f1b288f400aceedcae95879595fcdb6b5e1f
“dfgsa2” <dfgsa2@iscte-iul.pt>
2025-01-14 09:44:25 -0500

    add disable function to the Add Cart function

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -331,8 +331,7 @@ function ProductList() {
                         <div className="product-title">{plant.name}</div>
                         <div className="product-price">{plant.cost}</div>
                         <div className="product-description">{plant.description}</div>
-
-                        <button className="product-button" onClick={() => handleAddToCart(plant)}> Add plant to cart
+                        <button className={ addedToCart[plant.name] ? "btn-disabled" : "product-button"} onClick={() => !addedToCart[plant.name] && handleAddToCart(plant)} disabled = {addedToCart[plant.name]}> Add plant to cart
                         </button>
 
                     </div>
